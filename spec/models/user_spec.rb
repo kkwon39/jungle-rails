32429ea6cffe32fd05216b35e1d8b8d23be7614f
Raymond Kwan <rayk199492@gmail.com>
2018-10-15 10:54:48 -0400

    Added shoulda gem for rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject {
-    described_class.new()
-  }
 
   describe "Validations" do
+
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
+
+
   end
 end
