00cccd8536d9f6d93c8fd9428bbe3af2eb5eab30
Raymond Kwan <rayk199492@gmail.com>
2018-10-15 11:14:07 -0400

    Added test cases for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :reviews
 
   validates :first_name, :last_name, :email, :password_digest, presence: true
-
+  validates_uniqueness_of :email, :case_sensitive => false
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject {
+    described_class.new(
+      first_name: 'Test',
+      last_name: 'Test',
+      email: 'test@test.com',
+      password: '1234',
+      password_confirmation: '1234'
+      )
+  }
 
   describe "Validations" do
     it { should validate_presence_of(:first_name) }
@@ -8,5 +17,12 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
     it { should have_secure_password }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+  end
+
+  describe "New User" do
+    it "should be valid with matching password and password confirmation" do
+      expect(subject).to be_valid
+    end
   end
 end
